ba0561759ca284bd9a12771269cf613a64a65185
Naisthecreator <42411601+naisthecreator@users.noreply.github.com>
2025-09-11 02:04:24 +0000

    tested Jest

---
+++ app.js
@@ -0,0 +1,15 @@
+// We declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.07;
+    // return the dollar value
+    return valueInDollar;
+}
+
+const fromDollar
+
+const sum = (a,b)=>{
+    return a+b
+}
+console.log(sum(7,3));
+module.exports = {sum, fromEuroToDollar}
---
+++ test.js
@@ -0,0 +1,23 @@
+const {sum} = require('./app');
+test('adds 14 + 9 equal 23', () => {
+    let total = sum(14,9)
+    expect(total).toBe(23)
+
+})
+test("One euro should be 1.07 dollars", function() {
+    // Import the function from app.js
+    const { fromEuroToDollar } = require('./app.js');
+
+    // Use the function like its supposed to be used
+    const dollars = fromEuroToDollar(3.5);
+
+    // If 1 euro is 1.07 dollars, then 3.5 euros should be (3.5 * 1.07)
+    const expected = 3.5 * 1.07; 
+    
+    // This is the comparison for the unit test
+     expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07)
+})
+
+test("One dollar should be 156.5 Japanese Yen", function(){
+    const {fromDollarToYen} = require('./app.js');
+})
